80ed2bae21d22a3e01c87aae6b03fe67785679d4
jinsoru1464 <jis1464@naver.com>
2025-03-19 01:10:45 +0900

    25-03-19

---
+++ RestfulApi/src/main/java/com/example/RestfulApi/controller/HelloWorldController.java
@@ -0,0 +1,31 @@
+package com.example.RestfulApi.controller;
+
+import com.example.RestfulApi.bean.HelloWorldBean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    //GET
+    //URI -/hello-world
+    //@RequestMapping(method=RequestMethod.GET, path=" /hello-world")
+    @GetMapping(path = "/hello-world")
+    public String helloworld() {
+        return "Hello World";
+    }
+
+    @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean helloworldBean() {
+        return new HelloWorldBean("Hello World!");
+    }
+
+    @GetMapping(path = "/hello-world-bean/path-variable/{name}")
+    public HelloWorldBean helloWorldBeanPathVariable(@PathVariable String name){
+        return new HelloWorldBean(String.format("Hello world, %s", name));
+    }
+
+
+
+
+}
---
+++ RestfulApi/src/main/java/com/example/RestfulApi/dao/UserDaoService.java
@@ -0,0 +1,50 @@
+package com.example.RestfulApi.dao;
+
+import com.example.RestfulApi.bean.User;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Component
+public class UserDaoService {
+    private static List<User> users = new ArrayList<>();
+
+    private static int usersCount = 3;
+
+    static {
+        users.add(new User(1, "Jinsol Lee", new Date()));
+        users.add(new User(2, "Jin Lee", new Date()));
+        users.add(new User(3, "Alice Ryu", new Date()));
+    }
+
+
+    public List<User> findAll() {
+        return users;
+    }
+
+    public User save(User user) {
+        if (user.getId() == null) {
+            user.setId(++usersCount);
+        }
+
+        if (user.getJoinDate() == null) {
+            user.setJoinDate(new Date());
+        }
+
+        users.add(user);
+
+        return user;
+    }
+    public User findOne(int id) {
+        for (User user : users) {
+            if (user.getId() == id) {
+                return user;
+            }
+        }
+
+        return null;
+    }
+
+}
